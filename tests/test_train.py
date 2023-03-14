ad232d362064a1503c2e5f2e87574b1d77dddfeb
Vladimir Rusakov <24524555+wervlad@users.noreply.github.com>
2023-03-14 05:07:01 +0400

    add test for failure when test split ratio is greater then 1

---
+++ tests/test_train.py
@@ -0,0 +1,20 @@
+from click.testing import CliRunner
+import pytest
+
+from customer_churn_prediction.train import train
+
+
+@pytest.fixture
+def runner() -> CliRunner:
+    """Fixture providing click runner."""
+    return CliRunner()
+
+
+def test_error_for_invalid_test_split_ratio(
+    runner: CliRunner
+) -> None:
+    """It fails when test split ratio is greater than 1."""
+    result = runner.invoke(train, ["--test-split-ratio", 2])
+    assert result.exit_code == 2
+    assert "Invalid value for '--test-split-ratio'" in result.output
+
