7b99b60a00ab45d37f0d2d6cf5a6a04a10734589
Vladimir Rusakov <24524555+wervlad@users.noreply.github.com>
2023-03-15 06:11:31 +0400

    refactor model.py

--- src/customer_churn_prediction/model.py
+++ src/customer_churn_prediction/model.py
@@ -1,46 +1,258 @@
-from abc import ABCMeta, abstractproperty
+from abc import ABC, abstractmethod
+import git
 import numpy as np
+import mlflow
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import LogisticRegression as LogReg
+from sklearn.exceptions import NotFittedError
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.pipeline import Pipeline
 
 
-class Model(metaclass=ABCMeta):
-    def __init__(self, random_state=0):
+class MLflowModel(ABC):
+    """
+    A base class for creating machine learning models with MLflow support.
+
+    Attributes:
+        mlflow_tracking_uri: str
+            The URI of the MLflow server to use for logging.
+        mlflow_experiment_name: str
+            The name of the MLflow experiment to log to.
+        pipeline: Optional[Union[Pipeline, Callable[..., Pipeline]]]
+            An optional pipeline for feature preprocessing.
+            If provided, the model will be added to this pipeline during
+            initialization.
+        random_state: Optional[Union[int, RandomState]]
+            An optional random state to use for reproducible results.
+    """
+
+    def __init__(
+        self,
+        mlflow_tracking_uri="http://0.0.0.0:5000/",
+        mlflow_experiment_name=None,
+        pipeline=None,
+        random_state=0,
+    ):
         self.random_state = random_state
+        self.mlflow_tracking_uri = mlflow_tracking_uri
+        self.mlflow_experiment_name = mlflow_experiment_name
+        self.pipeline = pipeline if pipeline else Pipeline([])
+        self.pipeline.steps.append(("model", self.estimator))
 
-    @abstractproperty
-    def estimator(self):
-        pass
-
-    @abstractproperty
-    def params_grid(self):
-        pass
-
-
-class LogisticRegression(Model):
-    @property
-    def estimator(self):
-        return LogReg(random_state=self.random_state)
+        self.name = None
+        self.best_estimator = None
+        self.last_run_id = None
+        self.experiment_id = None
 
     @property
-    def params_grid(self):
-        return {
-            "model__C": np.arange(0.001, 100, 0.001),
-            "model__penalty": ["l1"],
-            "model__solver": ["saga"],
-            "model__max_iter": [1000],
+    @abstractmethod
+    def estimator(self):
+        """
+        An estimator object that will be used for training and evaluation.
+
+        Returns:
+            object: An instance of an estimator class.
+        """
+        pass
+
+    def train_with_logging(self, X, y, run_name=None):
+        """
+        Trains the model on the given data with logging enabled.
+
+        Parameters:
+        - X: Input data for training
+        - y: Target variable for training
+        """
+        mlflow.set_tracking_uri(self.mlflow_tracking_uri)
+        mlflow.sklearn.autolog()
+
+        repo = git.Repo(search_parent_directories=True)
+        version = repo.head.object.hexsha
+
+        with mlflow.start_run(
+            experiment_id=self.experiment_id,
+            run_id=self.last_run_id,
+            run_name=run_name,
+            tags={"mlflow.source.git.commit": version},
+        ) as run:
+            self.last_run_id = run.info.run_id
+            self.experiment_id = run.info.experiment_id
+
+            grid_search = GridSearchCV(
+                estimator=self.pipeline,
+                param_grid=self.param_grid,
+                scoring="roc_auc",
+                n_jobs=-1,
+                cv=10,
+                refit=True,
+            )
+            self.best_estimator = grid_search.fit(X, y)
+
+        return self
+
+    @property
+    @abstractmethod
+    def param_grid(self):
+        """
+        A dictionary of hyperparameters and their values for hyperparameter
+        tuning.
+
+        Returns:
+            dict: A dictionary of hyperparameters and their values.
+        """
+        pass
+
+    def evaluate(self, X_val, y_val) -> int:
+        """
+        Evaluate the performance of the trained model on a validation data set.
+
+        Args:
+            X_val: numpy.ndarray or pandas.DataFrame
+                Validation data set features.
+            y_val: numpy.ndarray or pandas.Series
+                Validation data set target variable.
+
+        Returns:
+            int:
+                R-squared score of the predictions.
+        """
+        if self.best_estimator is None:
+            raise NotFittedError(
+                "This model instance is not fitted yet. Call train with "
+                "appropriate arguments before using this estimator."
+            )
+        with mlflow.start_run(
+            experiment_id=self.experiment_id,
+            run_id=self.last_run_id,
+        ) as run:
+            roc_auc = roc_auc_score(y_val, self.best_estimator.predict(X_val))
+            mlflow.log_metric("roc_auc", roc_auc)
+            return roc_auc
+
+
+class LogisticRegressionMLflow(MLflowModel):
+    """
+    A class for training and evaluating a logistic regression model with
+    MLflow autologging.
+
+    Attributes:
+        mlflow_tracking_uri: str
+            The URI of the MLflow server to use for logging.
+        mlflow_experiment_name: str
+            The name of the MLflow experiment to log to.
+        pipeline: Optional[Union[Pipeline, Callable[..., Pipeline]]]
+            An optional pipeline for feature preprocessing.
+            If provided, the model will be added to this pipeline during
+            initialization.
+        random_state: Optional[Union[int, RandomState]]
+            An optional random state to use for reproducible results.
+
+    Properties:
+        estimator:
+            The logistic regression estimator used for training and prediction.
+        param_grid:
+            A dictionary specifying the hyperparameter grid for hyperparameter
+            tuning.
+
+    Methods:
+        train_with_logging(X, y, run_name=None):
+            Train a logistic regression model with MLflow autologging enabled.
+        evaluate(X_val, y_val):
+            Evaluate the trained logistic regression model on a validation set.
+
+    Examples:
+        >>> from sklearn.datasets import load_iris
+        >>> X, y = load_iris(return_X_y=True)
+        >>> model = LogisticRegressionMLflow(random_state=42)
+        >>> model.train_with_logging(X, y)
+        >>> model.evaluate(X, y)
+    """
+
+    @property
+    def estimator(self):
+        """
+        The logistic regression estimator used for training and prediction.
+
+        Returns:
+            An instance of sklearn.linear_model.LogisticRegression.
+        """
+        return LogisticRegression(random_state=self.random_state)
+
+    @property
+    def param_grid(self):
+        """
+        A dictionary specifying the hyperparameter grid for hyperparameter
+        tuning.
+
+        Returns:
+            A dictionary with keys corresponding to the hyperparameters to
+            tune, and values corresponding to the ranges of values to try for
+            each hyperparameter.
+        """
+        params = {
+            "penalty": ["l1", "l2"],
+            "C": np.linspace(0.1, 2, 20),
+            "fit_intercept": [True, False],
+            "solver": ["saga"],
+            "max_iter": [500, 1000],
         }
+        return {"model__" + key: val for key, val in params.items()}
 
 
-class RandomForest(Model):
+class RandomForestMLflow(MLflowModel):
+    """
+    A class representing a random forest model trained with MLflow autologging
+    enabled.
+
+    Attributes:
+        mlflow_tracking_uri: str
+            The URI of the MLflow server to use for logging.
+        mlflow_experiment_name: str
+            The name of the MLflow experiment to log to.
+        pipeline: Optional[Union[Pipeline, Callable[..., Pipeline]]]
+            An optional pipeline for feature preprocessing.
+            If provided, the model will be added to this pipeline during
+            initialization.
+        random_state: Optional[Union[int, RandomState]]
+            An optional random state to use for reproducible results.
+
+    Properties:
+        estimator: sklearn.ensemble.RandomForestClassifier
+            The random forest estimator used for training and prediction.
+        param_grid: dict
+            The parameter grid for hyperparameters tuning.
+
+
+    Methods:
+        train_with_logging(X, y, run_name=None):
+            Train a random forest model with MLflow autologging enabled.
+        evaluate(X_val, y_val):
+            Evaluate the trained logistic regression model on a validation set.
+
+    Example:
+    --------
+    >>> rf_model = RandomForestMLflow(random_state=42)
+    >>> rf_model.train_with_logging(X_train, y_train)
+    >>> rf_model.evaluate(X_test, y_test)
+    """
+
     @property
     def estimator(self):
         return RandomForestClassifier(random_state=self.random_state)
 
     @property
-    def params_grid(self):
-        return {
-            "model__min_samples_split": range(2, 200),
-            "model__min_samples_leaf": range(1, 200),
-            "model__n_estimators": [200],
+    def param_grid(self):
+        """
+        A dictionary of hyperparameters and their values for hyperparameter
+        tuning.
+
+        Returns:
+            dict: A dictionary of hyperparameters and their values.
+        """
+        params = {
+            "min_samples_split": range(2, 200),
+            "min_samples_leaf": range(1, 200),
+            "n_estimators": [200],
         }
+        return {"model__" + key: val for key, val in params.items()}
--- src/customer_churn_prediction/train.py
+++ src/customer_churn_prediction/train.py
@@ -1,21 +1,14 @@
 from pathlib import Path
 
 import click
-import git
-import mlflow
-import mlflow.sklearn
 import pandas as pd
 from sklearn.compose import ColumnTransformer
-from sklearn.metrics import roc_auc_score
-from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 
 from .data import FeaturePreprocessor
-from .model import LogisticRegression
-
-REPO = git.Repo(search_parent_directories=True)
-VERSION = REPO.head.object.hexsha
+from .model import LogisticRegressionMLflow
 
 NUM_COLS = [
     "ClientPeriod",
@@ -59,7 +52,9 @@ TARGET_COL = "Churn"
     default=0.2,
     type=click.FloatRange(0, 1, min_open=True, max_open=True),
 )
-def train(dataset_path: Path, random_state: int, test_split_ratio: float) -> None:
+def train(
+    dataset_path: Path, random_state: int, test_split_ratio: float
+) -> None:
     dataset = pd.read_csv(dataset_path)
     click.echo(f"Dataset shape: {dataset.shape}.")
     features = dataset.drop(TARGET_COL, axis=1)
@@ -68,37 +63,23 @@ def train(dataset_path: Path, random_state: int, test_split_ratio: float) -> Non
         features, target, test_size=test_split_ratio, random_state=random_state
     )
 
-    mlflow.set_tracking_uri("http://0.0.0.0:5000/")
-    mlflow.sklearn.autolog()
-
-    with mlflow.start_run(tags={"mlflow.source.git.commit": VERSION}) as run:
-        model = LogisticRegression(random_state=random_state)
-        classifier = Pipeline(
-            steps=[
-                ("feature_preprocessor", FeaturePreprocessor()),
-                (
-                    "num_cat_preprocessor",
-                    ColumnTransformer(
-                        transformers=[
-                            ("num", StandardScaler(), NUM_COLS),
-                            ("cat", OneHotEncoder(handle_unknown="ignore"), CAT_COLS),
-                        ]
-                    ),
+    pipeline = Pipeline(
+        steps=[
+            ("feature_preprocessor", FeaturePreprocessor()),
+            (
+                "num_cat_preprocessor",
+                ColumnTransformer(
+                    transformers=[
+                        ("num", StandardScaler(), NUM_COLS),
+                        ("cat", OneHotEncoder(), CAT_COLS),
+                    ]
                 ),
-                ("model", model.estimator),
-            ]
-        )
-
-        grid_search = GridSearchCV(
-            estimator=classifier,
-            param_grid=model.params_grid,
-            scoring="roc_auc",
-            n_jobs=-1,
-            cv=10,
-            refit=True,
-        )
-        logreg = grid_search.fit(features_train, target_train)
-
-        roc_auc = roc_auc_score(target_val, logreg.predict(features_val))
-        mlflow.log_metric("roc_auc", roc_auc)
-        click.echo(f"ROC AUC score: {roc_auc}.")
+            ),
+        ]
+    )
+    model = LogisticRegressionMLflow(
+        pipeline=pipeline, random_state=random_state
+    )
+    model.train_with_logging(features_train, target_train)
+    roc_auc = model.evaluate(features_val, target_val)
+    click.echo(f"ROC AUC score: {roc_auc}.")
