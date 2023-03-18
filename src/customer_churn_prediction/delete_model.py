4d6f9bf32da4f390f4731ba3f7c8ae2e48bcb8ee
Vladimir Rusakov <24524555+wervlad@users.noreply.github.com>
2023-03-18 10:51:20 +0400

    add delete_model script

---
+++ src/customer_churn_prediction/delete_model.py
@@ -0,0 +1,25 @@
+import click
+from mlflow import MlflowClient
+
+
+@click.command()
+@click.option(
+    "--name",
+    type=str,
+    help="Model name in MLflow registry.",
+)
+@click.option(
+    "--version",
+    type=int,
+    default=None,
+    help="Model version.",
+)
+def delete_model(
+    name: str,
+    version: int,
+) -> None:
+    """Delete a model from MLflow registry."""
+    if version is not None:
+        MlflowClient().delete_model_version(name=name, version=version)
+    else:
+        MlflowClient().delete_registered_model(name=name)
