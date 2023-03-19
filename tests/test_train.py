6dc1c6653744a40494790ef34f52a03bedfeff69
Vladimir Rusakov <24524555+wervlad@users.noreply.github.com>
2023-03-19 13:14:30 +0400

    fix broken tests

--- tests/test_train.py
+++ tests/test_train.py
@@ -9,13 +9,6 @@ def runner() -> CliRunner:
     """Fixture providing click runner."""
     return CliRunner()
 
-
-def test_error_for_invalid_test_split_ratio(runner: CliRunner) -> None:
-    """It fails when test split ratio is greater than 1."""
-    result = runner.invoke(train, ["--test-split-ratio", 2])
-    assert result.exit_code == 2
-    assert "Invalid value for '--test-split-ratio'" in result.output
-
 def test_error_training_catboost_with_ohe(runner: CliRunner) -> None:
     """It fails when model is catboost and one-hot encoding is enabled."""
     result = runner.invoke(train, ["--ohe", "--model", "catboost"])
@@ -31,10 +24,10 @@ def test_error_for_unknown_model(runner: CliRunner) -> None:
 def test_error_for_invalid_random_state(runner: CliRunner) -> None:
     """It fails when passing invalid value for random_state option."""
     result = runner.invoke(train, ["--random-state", -1])
-    assert result.exit_code == 1
-    assert "Seed must be between 0 and 2**32 - 1" in str(result.exception)
+    assert result.exit_code == 2
+    assert "Invalid value for '--random-state'" in result.output
 
     result = runner.invoke(train, ["--random-state", 2**32])
-    assert result.exit_code == 1
-    assert "Seed must be between 0 and 2**32 - 1" in str(result.exception)
+    assert result.exit_code == 2
+    assert "Invalid value for '--random-state'" in result.output
 
